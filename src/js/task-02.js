76dc50debfc826617d2fde53679cb7c2fc2a8499
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-02-08 19:10:01 +0000

    task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+ingredients.forEach((elItem) => {
+  const ingredientItem = document.createElement("li");
+  // ingredientItem.innerHTML = elItem;
+  ingredientItem.textContent = elItem;
+  ingredientsRef.append(ingredientItem);
+});
