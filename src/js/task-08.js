2b800c786669f73c9c6114dcd8161c7a7067a483
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-02-11 18:51:24 +0000

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Всі поля мають бути заповнені");
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+
+  form.reset();
+}
